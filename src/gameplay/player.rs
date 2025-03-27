cc9df073da0c1ab3310fe546c19f7c3886d1d3b6
GianniBuoni <162598175+GianniBuoni@users.noreply.github.com>
2025-03-27 14:01:02 -0400

    add and handle player jump states

--- src/gameplay/player.rs
+++ src/gameplay/player.rs
@@ -4,6 +4,7 @@ use bevy_tnua_avian2d::*;
 pub(super) fn plugin(app: &mut App) {
     app.register_ldtk_entity::<PlauerBundle>("Player");
     app.init_resource::<PlayerInputVector>();
+    app.init_resource::<PlayerStateResource>();
     app.add_systems(Update, player_input.in_set(UpdateSets::RecordInput));
     app.add_systems(Update, player_controls.in_set(UpdateSets::Update));
 }
@@ -29,8 +30,21 @@ pub struct PlauerBundle {
 #[derive(Resource, Default)]
 struct PlayerInputVector(pub Vec2);
 
+#[derive(Resource, Default)]
+struct PlayerStateResource {
+    current: PlayerState,
+}
+
+#[derive(Default)]
+pub enum PlayerState {
+    #[default]
+    Idle,
+    Jump,
+}
+
 fn player_input(
     mut input_vector: ResMut<PlayerInputVector>,
+    mut player_state: ResMut<PlayerStateResource>,
     kb_input: Res<ButtonInput<KeyCode>>,
 ) {
     let left: f32 = if kb_input.any_pressed([
@@ -53,10 +67,19 @@ fn player_input(
     };
 
     input_vector.0.x = right - left;
+
+    if kb_input.pressed(KeyCode::Space) {
+        player_state.current = PlayerState::Jump;
+        return;
+    }
+
+    // reset play state at the end?
+    player_state.current = PlayerState::Idle;
 }
 
 fn player_controls(
     input_vector: Res<PlayerInputVector>,
+    player_state: Res<PlayerStateResource>,
     mut controller: Query<&mut TnuaController>,
 ) {
     let mut controller = get_single_mut!(controller);
@@ -67,4 +90,15 @@ fn player_controls(
         acceleration: 100.,
         ..default()
     });
+
+    // TODO rewrite to a match statement later when more state are happening
+    // TODO figuer out how the Tsuna state management works into this
+    if let PlayerState::Jump = player_state.current {
+        controller.action(TnuaBuiltinJump {
+            height: GRAVITY * 0.75,
+            takeoff_extra_gravity: GRAVITY / 2.,
+            fall_extra_gravity: GRAVITY * 4.,
+            ..default()
+        })
+    }
 }
