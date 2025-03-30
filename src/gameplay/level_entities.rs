137839b3ae0467c3890de45f903ee810644ebc8a
GianniBuoni <162598175+GianniBuoni@users.noreply.github.com>
2025-03-30 18:59:58 -0400

    add decor entities

--- src/gameplay/level_entities.rs
+++ src/gameplay/level_entities.rs
@@ -2,6 +2,12 @@ use crate::prelude::*;
 
 pub(super) fn plugin(app: &mut App) {
     app.register_ldtk_entity::<PalmBundle>("PalmTop");
+    app.register_ldtk_entity::<DecorBundle>("Door");
+    app.register_ldtk_entity::<DecorBundle>("Candle");
+    app.register_ldtk_entity::<DecorBundle>("CandleLight");
+    app.register_ldtk_entity::<DecorBundle>("Window");
+    app.register_ldtk_entity::<DecorBundle>("ChainSmall");
+    app.register_ldtk_entity::<DecorBundle>("ChainBig");
 }
 
 #[derive(Component, Default)]
@@ -15,4 +21,10 @@ struct PalmBundle {
     sprite: Sprite,
     #[from_entity_instance]
     animation_config: AnimationConfig,
+}
+
+#[derive(LdtkEntity, Bundle, Default)]
+struct DecorBundle {
+    #[sprite_sheet]
+    sprite: Sprite,
 }
